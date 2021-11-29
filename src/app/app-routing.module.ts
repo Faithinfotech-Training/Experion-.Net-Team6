8f3b8d646dbf5a3f6766e58ad859f7496efa5763
ashikahzain <ashika.hussain@experionglobal.com>
2021-11-29 13:41:03 +0530

    components added and routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{LoginComponent} from './login/login.component';
+import {DoctorComponent} from './doctor/doctor.component';
 import { LabReportComponent } from './lab-report/lab-report.component';
 import { LabTechnicianComponent } from './lab-technician/lab-technician.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'doctor',component:DoctorComponent},
   {path:'lab',component:LabTechnicianComponent},
   {path:'report',component:LabReportComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LabTechnicianComponent } from './lab-technician/lab-technician.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { LabReportComponent } from './lab-report/lab-report.component';
+import { LoginComponent } from './login/login.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    DoctorComponent,
     LabTechnicianComponent,
     LabReportComponent
   ],
@@ -21,7 +23,8 @@ import { LabReportComponent } from './lab-report/lab-report.component';
     AppRoutingModule,
     FormsModule,
     NgxPaginationModule,
-    Ng2SearchPipeModule
+    Ng2SearchPipeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
