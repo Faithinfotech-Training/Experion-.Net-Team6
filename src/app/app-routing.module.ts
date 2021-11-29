24372e1bffe047a3cb3f10af97cb45fca3beb498
Sujith-Sk-work <sujith.kumar@experionglobal.com>
2021-11-29 12:56:18 +0530

    Added lab report and lab technician

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LabReportComponent } from './lab-report/lab-report.component';
+import { LabTechnicianComponent } from './lab-technician/lab-technician.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'lab',component:LabTechnicianComponent},
+  {path:'report',component:LabReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LabTechnicianComponent } from './lab-technician/lab-technician.component';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { LabReportComponent } from './lab-report/lab-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LabTechnicianComponent,
+    LabReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
