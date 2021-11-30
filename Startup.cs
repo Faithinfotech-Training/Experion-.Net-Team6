791719391cd393df17d18feee924bf73ddf92c9b
akhilpvijayan1 <akhil.vijayan@experionglobal.com>
2021-11-30 12:33:57 +0530

    Committ

--- Startup.cs
+++ Startup.cs
@@ -33,11 +33,19 @@ namespace cmsRestApi
             services.AddScoped<IDoctorRepository, DoctorRepository>();
             services.AddScoped<IPaymentRepo, PaymentRepo>();
             services.AddScoped<IPrescMedicineRepo, PrescMedicineRepo>();
+            services.AddScoped<IMedicineRepo, MedicineRepo>();
+            services.AddScoped<IPatientRepo, PatientRepo>();
+
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200", "https://b674-103-151-188-91.ngrok.io")
+            .AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
