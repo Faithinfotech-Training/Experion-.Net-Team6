4dd7f6c1454aa2aff646c55aaefc46f8ebf73997
AneesAB <mohammed.ab@experionglobal.com>
2021-12-01 14:15:12 +0530

    commit before pull

--- src/app/shared/admin.service.ts
+++ src/app/shared/admin.service.ts
@@ -24,7 +24,7 @@ export class AdminService {
 
   //get all doctor
   getalldoctor() {
-    this.httpClient.get(environment.apiUrl + "api/doctor/getalldoctor")
+    this.httpClient.get(environment.apiUrl + "/api/doctor/getalldoctor")
       .toPromise().then(
           result => this.doctors = result as Doctor[]
       )
@@ -39,7 +39,7 @@ export class AdminService {
   }
 
   getallSpecial() {
-    this.httpClient.get(environment.apiUrl + "api/doctor/getallspecialization") 
+    this.httpClient.get(environment.apiUrl + "/api/doctor/getallspecialization") 
     .toPromise().then(
       result => this.special = result as Special[]
     )
@@ -48,27 +48,27 @@ export class AdminService {
 
 
   insertdoctor(doctor: Doctor): Observable<any> {
-    return this.httpClient.post(environment.apiUrl + "api/doctor/Adddoctor", doctor);
+    return this.httpClient.post(environment.apiUrl + "/api/doctor/Adddoctor", doctor);
   }
 
   updatedoctor(doctor: Doctor): Observable<any> {
-    return this.httpClient.put(environment.apiUrl + "api/doctor/putdoctor", doctor);
+    return this.httpClient.put(environment.apiUrl + "/api/doctor/putdoctor", doctor);
   }
 
   insertstaff(staff: Staff): Observable<any> {
-    return this.httpClient.post(environment.apiUrl + "api/staff/Addstaff", staff);
+    return this.httpClient.post(environment.apiUrl + "/api/staff/Addstaff", staff);
   }
 
   updatestaff(staff: Staff): Observable<any> {
-    return this.httpClient.put(environment.apiUrl + "api/staff/putstaff", staff);
+    return this.httpClient.put(environment.apiUrl + "/api/staff/putstaff", staff);
   }
 
   getstaff(id: number): Observable<any> {
-    return this.httpClient.get(environment.apiUrl + "api/staff/GetStaff/" + id);
+    return this.httpClient.get(environment.apiUrl + "/api/staff/GetStaff/" + id);
   }
 
   getdoctor(id: number): Observable<any> {
-    return this.httpClient.get(environment.apiUrl + "api/doctor/GetADoctor/" + id);
+    return this.httpClient.get(environment.apiUrl + "/api/doctor/GetADoctor/" + id);
   }
 
 }
