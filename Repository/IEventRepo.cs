a29d1f749727828803e42c46f08dfed4c0a70c43
akhilpvijayan1 <akhil.vijayan@experionglobal.com>
2021-12-03 10:40:47 +0530

    C

---
+++ Repository/IEventRepo.cs
@@ -0,0 +1,24 @@
+﻿using cmsRestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cmsRestApi.Repository
+{
+    public interface IEventRepo
+    {
+        //--- get event by id ---//
+        Task<TblEvents> GetEventById(int id);
+        //--- get events  ---//
+        Task<TblEvents> getEvent(int id);
+        //--- get patients ---//
+        Task<List<TblEvents>> GetEvents();
+        //--- add Patient ---//
+        Task<TblEvents> AddEvent(TblEvents events);
+
+        //--- update Patient ---//
+        Task<TblEvents> UpdateEvent(TblEvents events);
+        Task<TblEvents> UpdateEventByActive(int id);
+    }
+}
